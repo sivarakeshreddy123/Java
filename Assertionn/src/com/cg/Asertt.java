07945aeb708cf4f9089851d970c8476c3b2da380
user1 <user1@user1-PC>
2019-09-25 19:43:43 +0530

    first commit uploading all files

---
+++ Assertionn/src/com/cg/Asertt.java
@@ -0,0 +1,11 @@
+package com.cg;
+
+public class Asertt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x=10;
+assert(x>10):++x;
+System.out.println(x);
+	}
+}
